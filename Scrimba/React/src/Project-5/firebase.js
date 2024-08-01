16a122e480d66e4e0025867a2dd7b63471da59ae
Bhoomi Gohil <bhoomi_gohil@hotmail.com>
2024-08-01 09:42:12 -0400

    Update firebase.js

--- Scrimba/React/src/Project-5/firebase.js
+++ Scrimba/React/src/Project-5/firebase.js
@@ -7,12 +7,12 @@ import { collection, getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnSifBCtz7_oIfSNtcrNnViv3XYW7EJTA",
-  authDomain: "react-notes-11d21.firebaseapp.com",
-  projectId: "react-notes-11d21",
-  storageBucket: "react-notes-11d21.appspot.com",
-  messagingSenderId: "6394946939",
-  appId: "1:6394946939:web:10ac6cd35f42b1add789de",
+  apiKey: "API-Key",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
